1503ec441debed6aec26090da9dcad3d8dd6fa51
Priyanshu Kumar <priyanshu2274@gmail.com>
2025-11-04 04:21:00 -0500

    Add files via upload

---
+++ Assigment/1stques.py
@@ -0,0 +1,18 @@
+"""
+Write a Python program that does the following:
+1.  Takes two numbers as input from the user.
+2.  Performs the basic mathematical operations on these two numbers:
+o	Addition
+o	Subtraction
+o	Multiplication
+o	Division
+3.  Displays the results of each operation on the screen.
+
+"""
+a = int(input("Enter your first number :"))
+b = int(input("Enter your second number :"))
+
+print("Addition : ", a+b)
+print("Subtraction :", a-b)
+print("Multiplication :" , a*b)
+print("Division :", a/b)
---
+++ Assigment/2ndques.py
@@ -0,0 +1,12 @@
+"""
+Write a Python program that:
+1.  Takes a user's first name and last name as input.
+2.  Concatenates the first name and last name into a full name.
+3.  Prints a personalized greeting message using the full name.
+
+"""
+
+first_name =  input("Enter your first name :")
+last_name = input("Enter your Last name :")
+
+print(" hello , ", first_name + " " + last_name , ", welcome to the world of code")
